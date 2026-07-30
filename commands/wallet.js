78786cd940da1b768807df53443a65dcb78925b7
Diego Cruces <DiegoCru1024@hotmail.com>
2026-07-29 21:23:48 -0500

    Update wallet.js

--- commands/wallet.js
+++ commands/wallet.js
@@ -7,19 +7,21 @@ module.exports = {
         .setDescription('Te muestra la información de tu billetera.'),
     async execute(interaction) {
         try {
-            const {currency} = await guildUserModel.findOne({
+            const guildUserData = await guildUserModel.findOne({
                 guildId: interaction.guild.id,
                 userId: interaction.user.id
             });
 
-            if (currency) {
-                const walletEmbed = new EmbedBuilder()
-                    .setColor(0x6400c8)
-                    .setTitle(`Billetera de ${interaction.user.username}`)
-                    .setDescription(`Tienes ${currency} Cosmic Coins :raccoon:`)
-                    .setThumbnail(interaction.user.avatarURL())
-                await interaction.reply({embeds: [walletEmbed]})
+            if (!guildUserData) {
+                return interaction.reply('Todavía no estás registrado. Usá algún comando de economía primero.');
             }
+
+            const walletEmbed = new EmbedBuilder()
+                .setColor(0x6400c8)
+                .setTitle(`Billetera de ${interaction.user.username}`)
+                .setDescription(`Tienes ${guildUserData.currency} Cosmic Coins :raccoon:`)
+                .setThumbnail(interaction.user.avatarURL())
+            return interaction.reply({embeds: [walletEmbed]})
         } catch (error) {
             console.error('Error al procesar el comando "wallet":', error);
             return interaction.reply('Ocurrió un error al procesar el comando.');
